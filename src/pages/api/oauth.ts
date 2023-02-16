f70256761930f8b7fe09e7dfabd48ece2363d987
Gnoompa <gnoompa@gmail.com>
2023-02-16 17:18:13 +0400

    dev

--- src/pages/api/oauth.ts
+++ src/pages/api/oauth.ts
@@ -1,13 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from "next/server";
 
 export const config = {
-  runtime: 'edge',
+  runtime: "edge",
 };
 
-export default (req: NextRequest) => {
-    const {id} = req.query
+export default async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
 
-  return NextResponse.json({
-    name: id,
-  });
+  const response = await fetch(
+    `https://api.twitter.com/2/oauth2/token?code=${code}&grant_type=authorization_code&redirect_uri=http://localhost:3001/redirect&code_verifier=challenge`,
+    {
+      headers: {
+        Authorization:
+          "Basic TmxsaVdXUXdlbnBvVFVoVlh6aHpYMEZuTUVzNk1UcGphUTpvaTA1VnVZeWVwa3dvQmZoVlJSeGpVQkp2dDRrQUM2SWprX1czOGs5VWtRVXlmVll5TQ==",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    }
+  );
+
+  return response;
 };
