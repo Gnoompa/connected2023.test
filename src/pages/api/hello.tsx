dd4b37f2aed8f39f6d6a94a507884748f39fd810
Gnoompa <gnoompa@gmail.com>
2023-03-28 18:00:08 +0400

    dev

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,16 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export const config = {
-  runtime: 'edge',
-}
+import { NextRequest } from "next/server";
 
-export default function handler(req, res) {
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
 
-  res
-    .status(200)
-    .send(
-      `<svg><image width='136' height='136' xlink:href='https://cyberconnect.me/_next/image?url=%2Fassets%2Fapp%2Flink3.png&w=128&q=75'/><text x="10" y="20" fill="red">${id}</text></svg>`
-    );
+  return new Response(
+    `<svg><image width='136' height='136' xlink:href='https://cyberconnect.me/_next/image?url=%2Fassets%2Fapp%2Flink3.png&w=128&q=75'/><text x="10" y="20" fill="red">${id}</text></svg>`,
+    {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
 }
