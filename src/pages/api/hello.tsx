f140c4d720faabcacea77e786a68f700a980ab87
Gnoompa <gnoompa@gmail.com>
2023-03-28 17:53:22 +0400

    dev

--- src/pages/api/hello.tsx
+++ src/pages/api/hello.tsx
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+export const config = {
+  runtime: 'edge',
+}
+
 export default function handler(req, res) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
